f2b10a8d03b5db1371c792f02cdd6e61572ae274
Adam Zerner <azerner3@gmail.com>
2014-11-12 10:37:54 -0500

    finished

--- main.js
+++ main.js
@@ -4,4 +4,5 @@ angular.module('randomNumberGenerator', [])
 		$scope.data.min = 0;
 		$scope.data.max = 10;
 		$scope.data.integers = true;
+		$scope.data.integersInput = "Math.floor(Math.random() * (" + $scope.data.max + "- " + $scope.data.min + " + 1)) + " + $scope.data.min + ";";
 	});
